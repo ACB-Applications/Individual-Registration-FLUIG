82be1df533c4cc006594b03d554bebd55c574e54
Marlinsk <Marlonnlmx@outlook.com>
2022-03-28 14:43:51 -0300

    Formatting and indenting code

--- Cadastro Individual/workflow/scripts/processo_cadastro_pessoa_fisica.beforeStateEntry.js
+++ Cadastro Individual/workflow/scripts/processo_cadastro_pessoa_fisica.beforeStateEntry.js
@@ -1,27 +1,22 @@
-function beforeStateEntry(sequenceId){
-	
-	
-	
-	if(sequenceId == 5){
-		
-		var attachments = hAPI.listAttachments();
-	    var hasAttachment = false;
-	  
-	    for (var i = 0; i < attachments.size(); i++) {
-	        var attachment = attachments.get(i);
-	        	        
-	        if (attachment.getDocumentDescription().indexOf(".pdf") > -1 ||
-	        	attachment.getDocumentDescription().indexOf(".png") > -1 ||
-	        	attachment.getDocumentDescription().indexOf(".jpeg") > -1) {
-	            hasAttachment = true;
-	        }
-	    }
-	  
-	    if (!hasAttachment) {
-	        throw "É obrigatório anexar arquivo de imagem referente ao cadastro!";
-	    }
-		
-		
-	}
-	
+function beforeStateEntry(sequenceId) {
+  if (sequenceId == 5) {
+    var attachments = hAPI.listAttachments();
+    var hasAttachment = false;
+
+    for (var i = 0; i < attachments.size(); i++) {
+      var attachment = attachments.get(i);
+
+      if (
+        attachment.getDocumentDescription().indexOf(".pdf") > -1 ||
+        attachment.getDocumentDescription().indexOf(".png") > -1 ||
+        attachment.getDocumentDescription().indexOf(".jpeg") > -1
+      ) {
+        hasAttachment = true;
+      }
+    }
+
+    if (!hasAttachment) {
+      throw "É obrigatório anexar arquivo de imagem referente ao cadastro!";
+    }
+  }
 }
--- Cadastro Individual/workflow/scripts/processo_cadastro_pessoa_fisica.servicetask19.js
+++ Cadastro Individual/workflow/scripts/processo_cadastro_pessoa_fisica.servicetask19.js
@@ -1,121 +1,126 @@
 function servicetask19() {
-	
-	var v_CPF = hAPI.getCardValue("CPF")+'';
-	
-	
-	v_CPF = v_CPF.replace(".","");
-	v_CPF = v_CPF.replace(".","");
-	v_CPF = v_CPF.replace("-","");
-	
-	
-	var v_nomecompleto = hAPI.getCardValue("nomecompleto")+'';
-	var v_nomeabreviado = hAPI.getCardValue("nomeabreviado")+'';
-	var v_email = hAPI.getCardValue("email")+'';
-	var v_telefonefixo = hAPI.getCardValue("telefonefixo")+'';
-	var v_telefonecelular = hAPI.getCardValue("telefonecelular")+'';
-	var v_CEP = parseInt(hAPI.getCardValue("CEP")+'');
-	var v_estado = hAPI.getCardValue("estado")+'';
-	var v_cidade = hAPI.getCardValue("cidade")+'';
-	var v_bairro = hAPI.getCardValue("bairro")+'';
-	var v_numero = parseInt(hAPI.getCardValue("numero")+'');
-	var v_rua = hAPI.getCardValue("rua")+'';
-	
-	
-	
-	 try{
- 		
-         var clientService = fluigAPI.getAuthorizeClientService();
-         
-         
-         /*
-         var dados =   {
-                 "cCgc": v_CPF,    
-                 "cNome": v_nomecompleto,    
-                 "nomeAbrev": v_nomeabreviado,           
-                 "cEmail": v_email,  
-                 "cFoneCel": v_telefonecelular,
-                 "cFoneFixo":v_telefonefixo,
-                 "iCep":v_CEP,    
-                 "cUF":v_estado,      
-                 "cCicade":v_cidade,  
-                 "cBairro":v_bairro,  
-                 "iNum":v_numero,    
-                 "cRua":v_rua    
-             };
-         */
-         
-         
-         
-         var dados  =   {
-                 "cCgc":"34053111013",    
-                 "cNome":"Teste fluig fixo",    
-                 "nomeAbrev":"CHE.GUE",           
-                 "cEmail":"che.guevara@totvs.com.br",  
-                 "cFoneCel":"47991285672",
-                 "cFoneFixo":"4612349876",
-                 "iCep":89665890,    
-                 "cUF":"SC",      
-                 "cCicade":"Joinville",  
-                 "cBairro":"Costa e Silva",  
-                 "iNum":150,    
-                 "cRua":"Rui Barbosa"    
-             };
-         
-         
-         
-         hAPI.setTaskComments(getValue("WKUser"), getValue("WKNumProces"),  0, "teste");
-         hAPI.setTaskComments(getValue("WKUser"), getValue("WKNumProces"),  0, JSON.stringify(dados));
-         
-         
-         var data = {
-             companyId : getValue("WKCompany") + '',
-             serviceCode : 'apiDatasul',
-             endpoint : '/dts/datasul-rest/resources/prg/esp/v1/esrest0001/implantaFornecedor',
-             method : 'post',// 'delete', 'patch', 'put', 'get'     
-             timeoutService: '100', // segundos
-          options : {
-              encoding : 'UTF-8',
-              mediaType: 'application/json',
-              useSSL:true,
-              crossDomain: true
-           },
-           params: dados
-         }
-         
-         hAPI.setTaskComments(getValue("WKUser"), getValue("WKNumProces"),  0, JSON.stringify(data));
-         
-         clientOauth = clientService.invoke(JSON.stringify(data));
-         
-         var result = clientOauth.getResult();
-   
-         if(result== null || result.isEmpty())
-        	 hAPI.setTaskComments(getValue("WKUser"), getValue("WKNumProces"),  0, "retorno vazio");
-         else
-        	 hAPI.setTaskComments(getValue("WKUser"), getValue("WKNumProces"),  0, result);
-         
-         //hAPI.setCardValue("retornoIntegracao",result);
-         
-         //if(result== null || result.isEmpty())
+  var v_CPF = hAPI.getCardValue("CPF") + "";
 
-         
-         log.info("result: " + result);
-         
-         var result = JSON.parse(clientOauth.getResult());
-         
-         hAPI.setCardValue("retornoIntegracao","Fim integracao");
+  v_CPF = v_CPF.replace(".", "");
+  v_CPF = v_CPF.replace(".", "");
+  v_CPF = v_CPF.replace("-", "");
 
-         return false; //provisorio
-         
-	}catch(e){
-		log.error("Ocorreu um erro incluir no Datasul: " + e);
+  var v_nomecompleto = hAPI.getCardValue("nomecompleto") + "";
+  var v_nomeabreviado = hAPI.getCardValue("nomeabreviado") + "";
+  var v_email = hAPI.getCardValue("email") + "";
+  var v_telefonefixo = hAPI.getCardValue("telefonefixo") + "";
+  var v_telefonecelular = hAPI.getCardValue("telefonecelular") + "";
+  var v_CEP = parseInt(hAPI.getCardValue("CEP") + "");
+  var v_estado = hAPI.getCardValue("estado") + "";
+  var v_cidade = hAPI.getCardValue("cidade") + "";
+  var v_bairro = hAPI.getCardValue("bairro") + "";
+  var v_numero = parseInt(hAPI.getCardValue("numero") + "");
+  var v_rua = hAPI.getCardValue("rua") + "";
 
-		hAPI.setCardValue("retornoIntegracao",e.message);
-		
-		return false;
-	
-	}          
-	
-	
-	
-	
+  try {
+    var clientService = fluigAPI.getAuthorizeClientService();
+
+    // var dados = {
+    //   cCgc: v_CPF,
+    //   cNome: v_nomecompleto,
+    //   nomeAbrev: v_nomeabreviado,
+    //   cEmail: v_email,
+    //   cFoneCel: v_telefonecelular,
+    //   cFoneFixo: v_telefonefixo,
+    //   iCep: v_CEP,
+    //   cUF: v_estado,
+    //   cCicade: v_cidade,
+    //   cBairro: v_bairro,
+    //   iNum: v_numero,
+    //   cRua: v_rua,
+    // };
+
+    // var dados = {
+    //   cCgc: "34053111013",
+    //   cNome: "Teste fluig fixo",
+    //   nomeAbrev: "CHE.GUE",
+    //   cEmail: "che.guevara@totvs.com.br",
+    //   cFoneCel: "47991285672",
+    //   cFoneFixo: "4612349876",
+    //   iCep: 89665890,
+    //   cUF: "SC",
+    //   cCicade: "Joinville",
+    //   cBairro: "Costa e Silva",
+    //   iNum: 150,
+    //   cRua: "Rui Barbosa",
+    // };
+
+    hAPI.setTaskComments(
+      getValue("WKUser"),
+      getValue("WKNumProces"),
+      0,
+      "teste"
+    );
+    hAPI.setTaskComments(
+      getValue("WKUser"),
+      getValue("WKNumProces"),
+      0,
+      JSON.stringify(dados)
+    );
+
+    var data = {
+      companyId: getValue("WKCompany") + "",
+      serviceCode: "apiDatasul",
+      endpoint:
+        "/dts/datasul-rest/resources/prg/esp/v1/esrest0001/implantaFornecedor",
+      method: "post", // 'delete', 'patch', 'put', 'get'
+      timeoutService: "100", // segundos
+      options: {
+        encoding: "UTF-8",
+        mediaType: "application/json",
+        useSSL: true,
+        crossDomain: true,
+      },
+      params: dados,
+    };
+
+    hAPI.setTaskComments(
+      getValue("WKUser"),
+      getValue("WKNumProces"),
+      0,
+      JSON.stringify(data)
+    );
+
+    clientOauth = clientService.invoke(JSON.stringify(data));
+
+    var result = clientOauth.getResult();
+
+    if (result == null || result.isEmpty())
+      hAPI.setTaskComments(
+        getValue("WKUser"),
+        getValue("WKNumProces"),
+        0,
+        "retorno vazio"
+      );
+    else
+      hAPI.setTaskComments(
+        getValue("WKUser"),
+        getValue("WKNumProces"),
+        0,
+        result
+      );
+
+    //hAPI.setCardValue("retornoIntegracao",result);
+
+    //if(result== null || result.isEmpty())
+
+    log.info("result: " + result);
+
+    var result = JSON.parse(clientOauth.getResult());
+
+    hAPI.setCardValue("retornoIntegracao", "Fim integracao");
+
+    return false; //provisorio
+  } catch (e) {
+    log.error("Ocorreu um erro incluir no Datasul: " + e);
+
+    hAPI.setCardValue("retornoIntegracao", e.message);
+
+    return false;
+  }
 }
