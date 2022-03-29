0785e22291b92209ad83877c4cd21812174be5b8
Marlon <Marlonnlmx@outlook.com>
2022-03-29 10:00:23 -0300

    Test using jquery in lockout.js file code

--- Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/lockout.js
+++ Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/lockout.js
@@ -4,75 +4,78 @@ var cidadeInput = document.querySelector("#cidade");
 var bairroInput = document.querySelector("#bairro");
 var ruaInput = document.querySelector("#rua");
 
-nomeCompletoInput.addEventListener("keypress", function(e) {
+$("[name=nomecompleto]").on("keypress", function () {
+  var regex = new RegExp("^[a-zA-Zàèìòùáéíóúâêîôûãõ\b]+$");
+  var _this = this;
 
-	if (!checkChar(e)) {
-		e.preventDefault();
-	}
+  setTimeout(function () {
+    var texto = $(_this).val();
+    if (!regex.test(texto)) {
+      $(_this).val(texto.substring(0, texto.length - 1));
+    }
+  }, 100);
 });
 
-estadoInput.addEventListener("keypress", function(e) {
+// nomeCompletoInput.addEventListener("keypress", function (e) {
+//   if (!checkChar(e)) {
+//     e.preventDefault();
+//   }
+// });
 
-	if (!checkChar(e)) {
-		e.preventDefault();
-	}
+estadoInput.addEventListener("keypress", function (e) {
+  if (!checkChar(e)) {
+    e.preventDefault();
+  }
 });
 
-cidadeInput.addEventListener("keypress", function(e) {
-
-	if (!checkChar(e)) {
-		e.preventDefault();
-	}
+cidadeInput.addEventListener("keypress", function (e) {
+  if (!checkChar(e)) {
+    e.preventDefault();
+  }
 });
 
-bairroInput.addEventListener("keypress", function(event) {
-
-	if (!checkCharNumber(event)) {
-		event.preventDefault();
-	}
+bairroInput.addEventListener("keypress", function (event) {
+  if (!checkCharNumber(event)) {
+    event.preventDefault();
+  }
 });
 
-ruaInput.addEventListener("keypress", function(event) {
-
-	if (!checkCharNumber(event)) {
-		event.preventDefault();
-	}
+ruaInput.addEventListener("keypress", function (event) {
+  if (!checkCharNumber(event)) {
+    event.preventDefault();
+  }
 });
 function checkChar(e) {
+  var char = String.fromCharCode(e.keyCode);
 
-	var char = String.fromCharCode(e.keyCode);
-
-	var pattern = '[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]';
-	if (char.match(pattern)) {
-		return true;
-	}
+  var pattern = "[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]";
+  if (char.match(pattern)) {
+    return true;
+  }
 }
 
 function checkCharNumber(event) {
+  var charNumber = String.fromCharCode(event.keyCode);
 
-	var charNumber = String.fromCharCode(event.keyCode);
-
-	var pattern = '[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9 ]';
-	if (charNumber.match(pattern)) {
-		return true;
-	}
+  var pattern = "[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9 ]";
+  if (charNumber.match(pattern)) {
+    return true;
+  }
 }
 
-var numero = document.querySelector("#numero")
+var numero = document.querySelector("#numero");
 
-numero.addEventListener("keypress", function(event) {
-
-	if (!checkCharSNumber(event)) {
-		event.preventDefault();
-	}
+numero.addEventListener("keypress", function (event) {
+  if (!checkCharSNumber(event)) {
+    event.preventDefault();
+  }
 });
 
 function checkCharSNumber(event) {
+  var charSNumber = String.fromCharCode(event.keyCode);
 
-	var charSNumber = String.fromCharCode(event.keyCode);
-
-	var pattern = '[A-Za-z0-9 ]';
-	if (charSNumber.match(pattern)) {
-		return true;
-	}
+  var pattern = "[A-Za-z0-9 ]";
+  if (charSNumber.match(pattern)) {
+    return true;
+  }
 }
