0fe403c4dff46499f305ccacc3182cbe30ada28d
Marlinsk <Marlonnlmx@outlook.com>
2022-03-29 14:59:19 -0300

    Remove coments

--- Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/maskInput.js
+++ Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/maskInput.js
@@ -1,7 +1,7 @@
-$("#CPF").mask("000.000.000-00") // CPF
-$("#telefone-celular").mask("+00 (00) 0 0000-0000") // Telefone celular
-$("#telefone-fixo").mask("+00 (00) 0000-0000") // Telefone fixo
-$("#CEP").mask("00000-000") // CEP
-$("#codigobanco").mask("000") // Código do banco
-$("#agencia").mask("0000") // Código da Agência
-$("#contacorrente").mask("0000000-0") // Código da Conta Corrente
+$("#CPF").mask("000.000.000-00") 
+$("#telefone-celular").mask("+00 (00) 0 0000-0000") 
+$("#telefone-fixo").mask("+00 (00) 0000-0000") 
+$("#CEP").mask("00000-000") 
+$("#codigobanco").mask("000") 
+$("#agencia").mask("0000") 
+$("#contacorrente").mask("0000000-0") 
