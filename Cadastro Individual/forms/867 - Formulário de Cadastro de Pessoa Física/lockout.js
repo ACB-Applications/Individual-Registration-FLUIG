f3ff0f8134c6ac78b3c185b476ed24de7c3ec895
Marlon <Marlonnlmx@outlook.com>
2022-04-01 13:37:53 -0300

    Fixing a declaration error

--- Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/lockout.js
+++ Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/lockout.js
@@ -21,7 +21,7 @@ $("[name=cidade],[name=bairro],[name=rua],[name=nomebanco]").on(
   }
 );
 
-$("[numero]").on("keypress", function (event) {
+$("[name=numero]").on("keypress", function (event) {
   var regex = new RegExp("^[a-zA-Z0-9 ]+$");
   var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
   if (!regex.test(key)) {
