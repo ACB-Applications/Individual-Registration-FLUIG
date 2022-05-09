8bb39009909b0ebb250627c536a2787907b22306
Marlinsk <Marlonnlmx@outlook.com>
2022-05-09 11:20:29 -0300

    Fixing maskInput.js

--- Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/rules/maskInput.js
+++ Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/rules/maskInput.js
@@ -1,7 +1,7 @@
-$("#CPF").mask("000.000.000-00") 
-$("#telefone-celular").mask("+00 (00) 0 0000-0000") 
-$("#telefone-fixo").mask("+00 (00) 0000-0000") 
-$("#CEP").mask("00000-000") 
-$("#codigobanco").mask("000") 
-$("#agencia").mask("0000") 
-$("#contacorrente").mask("0000000-0") 
+$("#CPF").mask("000.000.000-00");
+$("#telefone-celular").mask("(00) 0 0000-0000");
+$("#telefone-fixo").mask("(00) 0000-0000");
+$("#CEP").mask("00000-000");
+$("#codigobanco").mask("000");
+$("#agencia").mask("0000");
+$("#contacorrente").mask("0000000-0");
