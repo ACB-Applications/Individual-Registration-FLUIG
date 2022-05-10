e43311f1bb4cd282cd0f6ed519e2b031aecc3e34
Marlinsk <Marlonnlmx@outlook.com>
2022-05-10 09:13:59 -0300

    Changes

--- Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/events/enableFields.js
+++ Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/events/enableFields.js
@@ -1,34 +1,29 @@
 function enableFields(form) {
+  var ATIVIDADE_ATUAL = getValue("WKNumState");
 
-	var ATIVIDADE_ATUAL = getValue("WKNumState");
-
-	if (ATIVIDADE_ATUAL == 5) {
-		bloquearCampoInicial(form);
-	}
-	if (ATIVIDADE_ATUAL == 9) {
-		bloquearCampoAprovador(form);
-	}
-
+  if (ATIVIDADE_ATUAL == 5) {
+    bloquearCampoInicial(form);
+  }
+  if (ATIVIDADE_ATUAL == 9) {
+    bloquearCampoAprovador(form);
+  }
 }
 function bloquearCampoAprovador(form) {
-	form.setEnabled("observadorAprovador", false);
+  form.setEnabled("observadorAprovador", false);
 }
 function bloquearCampoInicial(form) {
-	
-	form.setEnabled("nomecompleto", false);
-	form.setEnabled("CPF", false);
-	form.setEnabled("email", false);
-	form.setEnabled("telefonecelular", false);
-	form.setEnabled("telefonefixo", false);
-	form.setEnabled("CEP", false);
-	form.setEnabled("estado", false);
-	form.setEnabled("cidade", false);
-	form.setEnabled("bairro", false);
-	form.setEnabled("numero", false);
-	form.setEnabled("rua", false);
-	form.setEnabled("nomebanco", false);
-	form.setEnabled("codigobanco", false);
-	form.setEnabled("agencia", false);
-	form.setEnabled("contacorrente", false);
-
+  form.setEnabled("nomecompleto", false);
+  form.setEnabled("CPF", false);
+  form.setEnabled("email", false);
+  form.setEnabled("telefonecelular", false);
+  form.setEnabled("telefonefixo", false);
+  form.setEnabled("CEP", false);
+  form.setEnabled("estado", false);
+  form.setEnabled("cidade", false);
+  form.setEnabled("bairro", false);
+  form.setEnabled("rua", false);
+  form.setEnabled("nomebanco", false);
+  form.setEnabled("codigobanco", false);
+  form.setEnabled("agencia", false);
+  form.setEnabled("contacorrente", false);
 }
