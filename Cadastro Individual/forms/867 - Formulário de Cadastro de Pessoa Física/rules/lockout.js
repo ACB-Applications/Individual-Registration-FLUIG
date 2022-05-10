037b14eb8f7a5c5474a47674ccb8334185e041ae
Marlinsk <Marlonnlmx@outlook.com>
2022-05-10 09:15:27 -0300

    Update

--- Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/rules/lockout.js
+++ Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/rules/lockout.js
@@ -7,16 +7,13 @@ $("[name=nomecompleto],[name=estado]").on("keypress", function () {
   }
 });
 
-$("[name=cidade],[name=bairro],[name=rua],[name=nomebanco]").on(
-  "keypress",
-  function (event) {
-    var regex = new RegExp("^[a-zA-Z0-9 ]+$");
-    var key = String.fromCharCode(
-      !event.charCode ? event.which : event.charCode
-    );
-    if (!regex.test(key)) {
-      event.preventDefault();
-      return false;
-    }
+$(
+  "[name=cidade],[name=bairro],[name=rua],[name=nomebanco],[name=contacorrente]"
+).on("keypress", function (event) {
+  var regex = new RegExp("^[a-zA-Z0-9 ]+$");
+  var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+  if (!regex.test(key)) {
+    event.preventDefault();
+    return false;
   }
-);
+});
