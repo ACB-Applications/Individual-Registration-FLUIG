277fe28f2f1c3ff66f8a0a775cc3229c21c86584
Marlinsk <Marlonnlmx@outlook.com>
2022-05-11 10:18:42 -0300

    Changing the directory name

--- Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/tasks/atividades.js
+++ Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/tasks/atividades.js
@@ -1,4 +1,5 @@
 $(function () {
+  esconderCampoAprovador();
   atividadeAtual();
 });
 
@@ -14,20 +15,24 @@ function atividades(ATIVIDADE_ATUAL) {
   if (ATIVIDADE_ATUAL == 12 || ATIVIDADE_ATUAL == 0) {
     sairCampo();
     feedbackToClient();
-    esconderAprovador();
+    aparecerInstrucaoUsuario();
+    aparecerInstrucaoAnexo();
     esconderCampoNomeAbreviado();
   } else if (ATIVIDADE_ATUAL == 5) {
-    aparecerAprovador();
+    mostrarCampoAprovador();
     aparecerCampoNomeAbreviado();
     esconderinputAnexo();
     regrasCampoAbreviado();
     sairCampoNomeAbreviado();
-    esconderCardInstrucaoUsuario();
-    esconderCardInstrucaoAnexo();
+    removerInstrucaoUsuario();
+    removerInstrucaoAnexo();
   } else if (ATIVIDADE_ATUAL == 9) {
     sairCampo();
     feedbackToClient();
+    aparecerInstrucaoUsuario();
+    aparecerInstrucaoAnexo();
     esconderCampoNomeAbreviado();
+    mostrarCampoAprovador();
   }
 }
 
@@ -155,6 +160,7 @@ function regrasCampoContaCorrente() {
   });
 }
 
+// Ação de clicar fora ou sair do campo
 function sairCampo() {
   $(
     "[name=CPF],[name=nomecompleto],[name=CEP],[name=estado],[name=cidade],[name=bairro],[name=rua],[name=nomebanco],[name=codigobanco],[name=agencia],[name=contacorrente]"
@@ -171,12 +177,26 @@ function sairCampoNomeAbreviado() {
   });
 }
 
-// Funções para mostrar os input
+// Mostra o input do nome abreviado
 function aparecerCampoNomeAbreviado() {
   $(".CampoNomeAbreviado").show();
 }
 
-function aparecerAprovador() {
+// Funções para mostrar as intruções
+function aparecerInstrucaoUsuario() {
+  $("[id=card-instrucao-usuario]").append(
+    '<div class="card-body"> <h3 class="card-title"> <i class="fluigicon fluigicon-warning-sign icon-sm"></i> Instruções para o usuário </h3> <p style="margin: 0px 0px 4px 0px;"> Siga abaixo as instruções e informações para não se perder na hora de preencher os campos do formulário: </p><ol style="padding: 0px 16px 0px 16px;"><li> Os campos com o símbolo<strong style="color: red;"> * </strong>são obrigatórios, caso contrário não tenha, são opcionais;</li><li>Os campos do formulário só aceitam digitos com letra Maiúscula;</li><li>Digite somente os números nos campos <strong>CPF, Telefone, CEP, Código, Agência, Conta Corrente</strong> que o formulário completa com a formatação correta;</li></ol></div>'
+  );
+}
+
+function aparecerInstrucaoAnexo() {
+  $("[id=card-instrucao-anexo]").append(
+    '<div class="card-body"> <h3 class="card-title"> <i class="fluigicon fluigicon-warning-sign icon-sm"></i> Instrução para o arquivo do anexo </h3> <p style="margin: 0px 0px 4px 0px;">Comprovante bancário válido. São os que contenham os dados de <strong>AGÊNCIA, CONTA, BANCO, TITULAR,</strong> sendo possível a perceber o vínculo bancário, como por ex: </p> <ul style="list-style: none; padding: 0px 0px 0px 0px;"> <li> - Folha de cheque;</li><li> - Foto do cartão do banco;</li><li> - Extrato de conta(ocultar dados financeiros);</li><li> - Print de tela do app do banco;</li><li> - Comprovante de transferência ou pix (ocultar dados financeiros);</li><li> - Formulário de abertura de conta;</li></ul></div>'
+  );
+}
+
+// Funções para mostrar o campo do feedback do aprovador
+function mostrarCampoAprovador() {
   $(".painelAprovador").show();
 }
 
@@ -185,20 +205,21 @@ function esconderCampoNomeAbreviado() {
   $(".CampoNomeAbreviado").hide();
 }
 
-function esconderCardInstrucaoUsuario() {
-  $("[id=card-instrucao-usuario]").hide();
+function esconderinputAnexo() {
+  $(".inputAnexo").hide();
 }
 
-function esconderCardInstrucaoAnexo() {
-  $("[id=card-instrucao-anexo]").hide();
-}
-
-function esconderAprovador() {
+function esconderCampoAprovador() {
   $(".painelAprovador").hide();
 }
 
-function esconderinputAnexo() {
-  $(".inputAnexo").hide();
+// Remove instruções
+function removerInstrucaoUsuario() {
+  $("[id=card-instrucao-usuario]").remove();
+}
+
+function removerInstrucaoAnexo() {
+  $("[id=card-instrucao-anexo]").remove();
 }
 
 // Show camera function
