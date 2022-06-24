dc96c56e18421d62f278e7498a5560deb3630319
Marlinsk <Marlonnlmx@outlook.com>
2022-06-24 14:38:50 -0300

    Feature: New function enabled to lock the field

--- Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/events/enableFields.js
+++ Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/events/enableFields.js
@@ -2,16 +2,24 @@ function enableFields(form) {
   var ATIVIDADE_ATUAL = getValue("WKNumState");
 
   if (ATIVIDADE_ATUAL == 5) {
-    bloquearCampoInicial(form);
+    bloquearCamposdePreenchimentopeloUsuario(form);
   }
+
   if (ATIVIDADE_ATUAL == 9) {
     bloquearCampoAprovador(form);
+  }
+
+  if (ATIVIDADE_ATUAL == 28) {
+    bloquearCampoRespostaIntegracao();
   }
 }
 function bloquearCampoAprovador(form) {
   form.setEnabled("observadorAprovador", false);
 }
-function bloquearCampoInicial(form) {
+function bloquearCampoRespostaIntegracao(form) {
+  form.setEnabled("RespostaIntegracao", false);
+}
+function bloquearCamposdePreenchimentopeloUsuario(form) {
   form.setEnabled("nomecompleto", false);
   form.setEnabled("CPF", false);
   form.setEnabled("email", false);
