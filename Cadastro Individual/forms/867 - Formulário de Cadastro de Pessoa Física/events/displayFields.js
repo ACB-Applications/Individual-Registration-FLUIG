6bccc9a5a7e98d06f89a01d07c2830a6e28d578a
Marlinsk <Marlonnlmx@outlook.com>
2022-09-12 20:40:04 -0300

    Some changes

--- Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/events/displayFields.js
+++ Cadastro Individual/forms/867 - Formulário de Cadastro de Pessoa Física/events/displayFields.js
@@ -1,14 +1,11 @@
 function displayFields(form, customHTML) {
-
 	recuperarAtividadeJS(form, customHTML);
-
 }
-function recuperarAtividadeJS(form, customHTML) {
 
+function recuperarAtividadeJS(form, customHTML) {
 	customHTML.append("<script>function buscarUsuarioLogado(){return " + "'" + getValue('WKUser') + "'" + ";}</script>");
 	customHTML.append("<script>function buscaEmpresa(){return " + getValue('WKCompany') + ";}</script>");
 	customHTML.append("<script>function buscarAtividadeAtual(){return " + getValue("WKNumState") + ";}</script>");
 	customHTML.append("<script>function buscarModoForm(){return '" + form.getFormMode() + "';}</script>");
 	customHTML.append("<script>function buscarIdSolicitacao(){return " + "'" + getValue('WKNumProces') + "'" + ";}</script>");
-
 }
